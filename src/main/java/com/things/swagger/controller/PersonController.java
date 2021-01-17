4dea8d2edf6a2c1bd7e9da868b1a4dec122e0aa5
d0 <dip.0cean@daum.net>
2021-01-18 01:28:40 +0900

    @RepositoryRestController에 대해 공부하기

--- src/main/java/com/things/swagger/controller/PersonController.java
+++ src/main/java/com/things/swagger/controller/PersonController.java
@@ -2,23 +2,33 @@ package com.things.swagger.controller;
 
 import com.things.swagger.domain.Person;
 import com.things.swagger.service.PersonService;
+import io.swagger.annotations.*;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.rest.webmvc.RepositoryRestController;
 import org.springframework.web.bind.annotation.*;
 
-@RestController
 @RequestMapping("/person")
+@Api(value = "/person", description = "Person 객체에 대한 CRUD")
 @RequiredArgsConstructor
+@RepositoryRestController
 public class PersonController {
 
     private final PersonService personService;
 
-    @GetMapping("/{id}")
+    @ApiOperation(value = "회원 검색", nickname = "회원 검색")
+    @RequestMapping(method = RequestMethod.GET, value = "/{id}")
+    @ApiImplicitParam(name = "id", dataType = "string", paramType = "query", value = "회원 고유번호")
+    @ApiResponses({
+            @ApiResponse(code = 200, message = "회원 조회 성공"),
+            @ApiResponse(code = 401, message = "회원 조회 실패", response = Person.class)
+    })
     public Person findByPerson(@PathVariable("id") String id) {
         return personService.findByAll(id);
 
     }
 
-    @PostMapping("/save")
+    @ApiOperation(value = "회원 등록", nickname = "회원 등록")
+    @RequestMapping(method = RequestMethod.POST, value = "/save")
     public Person save(@RequestBody Person person) {
         return personService.save(person);
     }
--- src/main/java/com/things/swagger/repository/PersonRepository.java
+++ src/main/java/com/things/swagger/repository/PersonRepository.java
@@ -2,9 +2,8 @@ package com.things.swagger.repository;
 
 import com.things.swagger.domain.Person;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 @RepositoryRestResource
-public interface PersonRepository extends JpaRepository<Person, String>, QuerydslPredicateExecutor<Person> {
+public interface PersonRepository extends JpaRepository<Person, String> {
 }
